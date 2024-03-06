9f162d6aaf0f3ea1a78920bf91ecf746b5d1f154
kaikaci12 <somewonesomewone@gmail.com>
2024-03-06 23:59:06 +0400

    Create Luhn's Algorigthm

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function checkSum() {
-  const splitedCard = creditCardNumber.split("");
-  const everySecondNum = splitedCard.filter((number, index) => index % 2 == 0);
-  console.log(everySecondNum);
+function checkCredictCard() {
+  let creditCardNumber;
+  let stringified;
 }
-checkSum();
-const creditCardNumber = parseInt(prompt("Credit Card Number:"));
