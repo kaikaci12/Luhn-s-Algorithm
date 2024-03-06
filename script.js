4938451e6a6bed2d647cf95521eb5842422eee0d
kaikaci12 <somewonesomewone@gmail.com>
2024-03-07 00:16:10 +0400

    add checksum function and and convert the card number to array

--- script.js
+++ script.js
@@ -48,3 +48,8 @@ function checkCardType(stringified) {
 
   return "Unknown Card Type";
 }
+
+function checkSum(creditCardNumber) {
+  const stringified = creditCardNumber.toString();
+  const splitedCard = stringified.split("");
+}
