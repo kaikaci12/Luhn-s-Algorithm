85eb62580958ca4f37c0320e0d62189fc43f0729
kaikaci12 <somewonesomewone@gmail.com>
2024-03-07 00:03:10 +0400

    start to set up credit card validation program

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function checkCredictCard() {
   let creditCardNumber;
   let stringified;
+
+  do {
+    creditCardNumber = parseInt(prompt("Credit Card Number:"));
+    stringified = creditCardNumber.toString();
+    if (
+      stringified.length < 13 ||
+      stringified.length > 16 ||
+      isNaN(creditCardNumber) ||
+      stringified.length == 14
+    ) {
+      console.log("Invalid card, try again:");
+    }
+  } while (
+    stringified.length < 13 ||
+    stringified.length > 16 ||
+    isNaN(creditCardNumber) ||
+    stringified.length == 14
+  );
+
+  return creditCardNumber;
 }
