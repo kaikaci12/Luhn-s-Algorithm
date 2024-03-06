638d0207522edfbd5ac58cccfe4fb0f18267768b
kaikaci12 <somewonesomewone@gmail.com>
2024-03-07 00:13:04 +0400

    finish up credit card validation program, this checks the card type

--- script.js
+++ script.js
@@ -20,5 +20,31 @@ function checkCredictCard() {
     stringified.length == 14
   );
 
-  return creditCardNumber;
+  return creditCardNumber; // Return the validated credit card number
+}
+
+function checkCardType(stringified) {
+  let firstTwoDigits;
+
+  if (stringified.length === 15) {
+    firstTwoDigits = stringified.slice(0, 2);
+    if (firstTwoDigits === "34" || firstTwoDigits === "37") {
+      return "American Express Card";
+    }
+  }
+  if (stringified.length === 16) {
+    firstTwoDigits = stringified.slice(0, 2);
+    if (firstTwoDigits >= "51" && firstTwoDigits <= "55") {
+      return "MasterCard";
+    }
+  }
+
+  if (stringified.length === 13 || stringified.length === 16) {
+    firstTwoDigits = stringified.slice(0, 1);
+    if (firstTwoDigits === "4") {
+      return "VISA";
+    }
+  }
+
+  return "Unknown Card Type";
 }
